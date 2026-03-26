154dd4d06d2f26d4e7b76a82d3ac37ddb835781e
Baggnan Seydou <baggnan678@gmail.com>
2026-03-26 20:38:22 +0000

    rajout exo 7

--- exercice 5.py
+++ exercice 5.py
@@ -2,7 +2,7 @@ produit=[str(input("entré un mot : ")) for _ in range(4)]
 verification = str(input("entré un mot à vérifier : "))
 verif=produit.count(verification)
 if verif==0:
-    print (f"Voici le tableau {produit} , voici la verification {verification} , et le mot n'est pas present")
+    print (f"Voici le tableau {produit} , voici le mot à vérifier {verification} , et le mot n'est pas present")
 else:
     index=produit.index(verification)
-    print (f"Voici le tableau {produit} , voici la verification {verification} , et le mot est present {verif} fois à l'index {index}")
+    print (f"Voici le tableau {produit} , voici le mot à vérifier {verification} , et le mot est present {verif} fois à l'index {index}")
---
+++ exercice 7.py
@@ -0,0 +1,4 @@
+tab=[int(input("entré une un élémrnt du 1er tableau : ")) for _ in range(6)]
+tableau2 = [int(input("entré une un élémrnt du 2eme tableau : ")) for _ in range(6)]
+tableau3 = [tab,tableau2]
+print (f"Voici le tableau {tab} , voici le 2eme tableau {tableau2} , voici les deux tableaux {tableau3}")
